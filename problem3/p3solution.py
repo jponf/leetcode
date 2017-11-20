4c8d48c848e63779ec2a36e8c333d4c7cb2fe297
Josep <jponfarreny@gmail.com>
2017-11-21 00:34:24 +0100

    Added problem 3 python solution

---
+++ problem3/p3solution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def length_of_longest_substring(text):
+    chars = {}  # type: MutableMapping[str, int]
+    best, first = 0, 0
+
+    for i, c in enumerate(text):
+        if c in chars:
+            best = max(best, i - first)
+            first = max(first, chars.pop(c) + 1)
+        chars[c] = i
+
+    return max(best, len(text) - first)
+
+
+if __name__ == '__main__':
+    text = "abcabcbb"
+
+    print("Length of longest substring in '%s'" % text)
+    print(length_of_longest_substring(text))
