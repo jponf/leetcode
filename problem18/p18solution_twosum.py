77fd23193d2ea47646411c9c132fe12f025f3b2c
Josep Pon Farreny <josep.pon-farreny@siemens.com>
2025-09-07 07:57:35 +0200

    Add problem description

--- problem18/p18solution_twosum.py
+++ problem18/p18solution_twosum.py
@@ -1,3 +1,26 @@
+"""Given an array nums of n integers, return an array of all the unique
+quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+    0 <= a, b, c, d < n
+    a, b, c, and d are distinct.
+    nums[a] + nums[b] + nums[c] + nums[d] == target
+
+You may return the answer in any order.
+
+Example 1:
+    Input: nums = [1,0,-1,0,-2,2], target = 0
+    Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+Example 2:
+    Input: nums = [2,2,2,2,2], target = 8
+    Output: [[2,2,2,2]]
+
+Constraints:
+    1 <= nums.length <= 200
+    -109 <= nums[i] <= 109
+    -109 <= target <= 109
+"""
+
 from typing import List
 
 
@@ -25,7 +48,7 @@ class Solution:
             if target > max_s:
                 return []
 
-            # Two sum variant or recursive k-sum
+            # 2-sum variant or recursive k-sum
             if k == 2:
                 return self.twoSumSorted(nums, target, left, nums_len - 1)
             else:
